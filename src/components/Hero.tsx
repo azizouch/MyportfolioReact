4ef90069ed084663af558188f8cd7d74123c4c2e
Your Name <abdelazizochwar@gmail.com>
2025-09-08 17:15:33 +0100

    fix responsive for mobile screen part2

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -96,13 +96,13 @@ const Hero = () => {
           </div>
         </div>
       </div>
-
       {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce mt-8 sm:mt-0">
         <div className="w-6 h-10 border-2 border-gray-400 rounded-full flex justify-center">
           <div className="w-1 h-3 bg-gray-400 rounded-full mt-2 animate-pulse"></div>
         </div>
       </div>
+      
     </section>
   );
 };
